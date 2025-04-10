19ee3a2506ecb5f07082341a62a6367d8149de7e
Kristijan Kosutic <kosutic.kristijan@gmail.com>
2025-04-10 19:52:26 +0200

    testing something

--- init.lua
+++ init.lua
@@ -726,7 +726,6 @@ require('lazy').setup({
         apex = { 'prettierd', 'prettier', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
