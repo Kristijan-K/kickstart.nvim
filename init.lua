ea7691176345da192f5c00336d589f47dbabd181
Kristijan Kosutic <kosutic.kristijan@gmail.com>
2025-04-10 19:42:50 +0200

    add prettier

--- init.lua
+++ init.lua
@@ -666,7 +666,10 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'prettier',
+        'prettierd',
+        'eslint', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -719,7 +722,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        apex = { 'prettierd', 'prettier', stop_after_first = true },
+        apexcode = { 'prettierd', 'prettier', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
