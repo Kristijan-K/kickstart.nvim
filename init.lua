46dd4c05381e006cc4223490ab021ae37a329b90
Kristijan Kosutic <kosutic.kristijan@gmail.com>
2025-04-25 11:43:39 +0200

    add oil

--- init.lua
+++ init.lua
@@ -214,6 +214,17 @@ require('lazy').setup({
     'ThePrimeagen/vim-be-good',
   },
   {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
